bc91436fdda9dda1d359c5708448e1cb03212f6e
fbt328 <katherine.rust.andrews@gmail.com>
2019-05-13 20:04:54 -0700

    barebones setup of the server. Server home message displaying as well as server listen port info message displaying

---
+++ index.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+const server = require('./server.js');
+PORT = process.env.PORT || 9001;
+
+server.listen(PORT, () => {
+    console.log(`BEEP BOOP I am a server, listening on PORT ${PORT}`)
+});
