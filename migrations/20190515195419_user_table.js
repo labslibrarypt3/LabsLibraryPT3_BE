b9813a50e8a32a8bdf7224abf18604e47b23a08b
unknown <tomutsey@yahoo.com>
2019-05-24 01:19:03 -0400

    Heruko functional pg partially functional working to get seeding debugged

--- migrations/20190515195419_user_table.js
+++ migrations/20190515195419_user_table.js
@@ -6,7 +6,7 @@
             table.string('firstName').notNullable();
             table.string('lastName').notNullable();
             table.string('email').unique().notNullable();
-            table.integer('phoneNumber').unique().notNullable();
+            table.string('phoneNumber').unique().notNullable();
             //include username and password
             // table.foreign('Book_id').references() come back to this when table is set up
     })
