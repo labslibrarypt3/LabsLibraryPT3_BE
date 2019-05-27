c0fa48cc0d2f96e5f5c437133a0431c9433a2e30
unknown <tom@edgegluer.com>
2019-05-26 23:28:51 -0400

    prepping endpoint to handle oath data from front end

--- routes/oauth/githubauth.js
+++ routes/oauth/githubauth.js
@@ -1,34 +1,12 @@
+
 const express = require('express');
-const request = require('superagent');
+const router = express.Router();
+const db = require('../../DATA/helpers/usersDb');
 
-
-module.exports = (auth) => {
-
-auth.get('/callback',(req, res, next) => {
-   
-      const { query } = req;
-      const { code } = code;
-
-      if (!code){
-          return res.send({
-              success: false,
-              message: 'Error: no code',
-          })
-        
-      }
-      
-      request
-      .post(`http://github.com/login/oauth/access_token`)
-      .send({
-          client_id: '66d10ed2a42e30acdfcb',
-          client_secret:'b78a4b174578404adddd85e5d7b620fd9713e109',
-          code: code
-      })
-      .set('Accept','application/json')
-      .then(function(result){
-          const data = result.body;
-          console.log(result)
-          res.send(data);
-      });
-    });
-};
+router.post('/callback',(req, res, next) => {
+    console.log(req.body)
+    
+    res.status(200).json(req.body);
+})
+    
+module.exports = router;
