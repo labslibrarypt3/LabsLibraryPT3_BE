3805a5f8881dc749b8a8d622960dd0825402b885
unknown <tom@edgegluer.com>
2019-05-29 00:08:35 -0400

    Updates to tables not migrated latest to heroku as of yet

--- routes/oauth/githubauth.js
+++ routes/oauth/githubauth.js
@@ -2,11 +2,26 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../DATA/helpers/usersDb');
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-router.post('/callback',(req, res, next) => {
+router.post('/callback',async (req, res) => {
     console.log(req.body)
-    
     res.status(200).json(req.body);
 })
     
 module.exports = router;
+
+// router.post('/add', async (req,res) => {
+//     console.log(req.body)
+//     const enter = req.body
+//       try {
+//         const user = await db.insert(enter);
+//         res.status(201).json(user);
+//       } catch (error) {
+//         // log error to database
+//         res.status(500).json({
+//           message: 'Error adding the User',
+//         });
+//       }
+//     });
