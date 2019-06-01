dabef9cd779c8de26d429f4ec4fa864d22266727
unknown <tom@edgegluer.com>
2019-06-01 06:55:03 -0400

    Add foreign key, status, timestamp of creation, timestamp of updates,due date

--- migrations/20190528223820_Books.js
+++ migrations/20190528223820_Books.js
@@ -2,9 +2,27 @@
 exports.up = function(knex) {
     return knex.schema.createTable('Books', table => {
         table.increments();
-        table.integer('Library')
-        table.integer('Borrowed')
-        table.integer('Lent')
+        table.string('Library')
+             .notNullable();
+        table.string('status')
+             .notNullable();
+        table
+             .timestamp("created_at")
+             .notNullable()
+             
+
+        table
+             .timestamp("updated_at")
+             .notNullable()
+        
+        table
+             .string("Due date")
+             .notNullable()
+             
+        table.bigInteger('user_id')
+             .references('id')
+             .inTable('Users')
+            
 });
 }
 
--- routes/user-endpoints.js
+++ routes/user-endpoints.js
@@ -30,4 +30,6 @@ router.post('/add', async (req,res) => {
       });
     }
   });
+
+  
 module.exports = router; 
