840cacbfefa141c94c6dfdbc0a63d427a1b9da94
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-05 20:57:56 -0700

    Added Generic POST in user-endpoints.js and some notes

--- routes/user-endpoints.js
+++ routes/user-endpoints.js
@@ -1,53 +1,59 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../DATA/helpers/usersDb');
-const jwt = require ('jsonwebtoken');
-const ejwt = require('express-jwt');
+const db = require("../DATA/helpers/usersDb");
+const jwt = require("jsonwebtoken");
+const ejwt = require("express-jwt");
 
 // view list of users
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const user = await db.get(req.query);
     res.status(200).json(user);
-  } catch (error) { 
+  } catch (error) {
     console.log(error);
     res.status(500).json({
-      message: 'Error retrieving the Users',
+      message: "Error retrieving the Users"
     });
   }
-})
-
+});
 
 // Add a user with post
-router.post('/add', async (req,res) => {
-  console.log(req.body)
-  const enter = req.body
-    try {
-      const user = await db.insert(enter);
-      res.status(201).json(user);
-    } catch (error) {
-      // log error to database
-      res.status(500).json({
-        message: 'Error adding the User',
-      });
-    }
-  });
+router.post("/add", async (req, res) => {
+  console.log(req.body);
+  const enter = req.body;
+  try {
+    const user = await db.insert(enter);
+    res.status(201).json(user);
+  } catch (error) {
+    // log error to database
+    res.status(500).json({
+      message: "Error adding the User"
+    });
+  }
+});
 
-  router.get("/account", (req, res) => {
-    res.send("account page here");
-  });
-  
-  router.get("/mybookshelf", (req, res) => {
-    res.send("mybookshelf page here");
-  });
-  
-  router.get("/books", (req, res) => {
-    res.send("books page here");
-  });
-  
-  router.get("/users", (req, res) => {
-    res.send("users page here");
-  });
+router.get("/account", (req, res) => {
+  res.send("account page here");
+});
 
-  
-module.exports = router; 
+router.get("/mybookshelf", (req, res) => {
+  res.send("mybookshelf page here");
+});
+
+router.get("/books", (req, res) => {
+  res.send("books page here");
+});
+
+router.get("/users", (req, res) => {
+  res.send("users page here");
+});
+
+//add book to personal library
+router.post("/users/:id/add-book", (res, res) => {
+  //use "getByID" to find the right spot in the db
+    // double check training kit to see if getByID is necessary here or if we already should have that data due to the URL
+  //use "insert" to add a book object to the library
+  res.send("User can now lend this book out to neighbs")
+})
+
+module.exports = router;
