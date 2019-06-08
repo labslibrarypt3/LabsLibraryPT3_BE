d63abac53ed52594286b0021537b29136665a55b
unknown <tom@edgegluer.com>
2019-06-08 13:09:27 -0400

    add user foreign key to books table to allow retrieval and reference  of user library

--- migrations/20190528223820_Books.js
+++ migrations/20190528223820_Books.js
@@ -2,13 +2,19 @@
 exports.up = function(knex) {
     return knex.schema.createTable('Books', table => {
         table.increments('bookId');
+        
         table.string('title')
              .notNullable();
+
         table.string('authors')
              .notNullable();
+
         table.integer('ISBN')
              .notNullable()  
-   
+
+        table.bigInteger('user_id')
+             .references('userId')
+             .inTable('Users')
             
 });
 }
