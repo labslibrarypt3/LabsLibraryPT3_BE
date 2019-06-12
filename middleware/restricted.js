4e0501ff165dd4661bb4cfefcb5a5b6ae4ccecb4
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-11 23:14:24 -0700

    Add restricted middleware and implement it on goodreads route

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,7 +1,22 @@
+//this verifies the oauth access token
+module.exports = {
+  restricted
+};
+
 function restricted(req, res, next) {
-  if (req.session && req.session.userId) {
+  //get auth header value
+  const bearerHeader = req.headers["authorization"];
+  //check if bearer header is undefined
+  if (typeof bearerHeader !== "undefined") {
+    //split bearer header value "Bearer __token_here__" at the space in the string
+    const bearer = bearerHeader.split(" ");
+    //split returns an array. get token from array
+    const bearerToken = bearer[1];
+    //set token
+    req.token = bearerToken;
+    //pass baton to next middleware
     next();
   } else {
-    res.status(401).json({ message: "You are not logged in" });
+    res.sendStatus(403).json({ error: "forbidden" });
   }
 }
--- routes/goodreads.js
+++ routes/goodreads.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const db = require("../DATA/dbConfig");
 const router = express.Router();
-// add goodreadsBooksModel
+const restricted = require("../middleware/restricted");
 
 const request = require("request-promise");
 const { parseString } = require("xml2js");
 
-router.get("/search", (req, res) => {
-  request
-    .get(
-      `https://www.goodreads.com/search/index.xml?key=${
-        process.env.GOODREADS_KEY
-      }&q=${req.query.q}`
-    )
-    .then(result =>
-      parseString(result, (err, goodreadsResult) =>
-        res.json({
-          books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
-            work => ({
-              goodreadsId: work.best_book[0].id[0]._,
-              title: work.best_book[0].title[0],
-              authors: work.best_book[0].author[0].name[0],
-              covers: [work.best_book[0].image_url[0]]
-              // isbn: work.best_book[0].i
+router.get("/search", restricted, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, userData) => {
+    if (err) {
+      res.sendStatus(403).json({ error: "forbidden" });
+    } else {
+      request
+        .get(
+          `https://www.goodreads.com/search/index.xml?key=${
+            process.env.GOODREADS_KEY
+          }&q=${req.query.q}`
+        )
+        .then(result =>
+          parseString(result, (err, goodreadsResult) =>
+            res.json({
+              books: goodreadsResult.GoodreadsResponse.search[0].results[0].work.map(
+                work => ({
+                  goodreadsId: work.best_book[0].id[0]._,
+                  title: work.best_book[0].title[0],
+                  authors: work.best_book[0].author[0].name[0],
+                  covers: [work.best_book[0].image_url[0]]
+                  // isbn: work.best_book[0].i
+                })
+              )
             })
           )
-        })
-      )
-    );
+        );
+    }
+  });
 });
 
 // route to add a book to personal library so that one can lend it out will be in user-endpoints.js
