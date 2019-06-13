91f7dd2072a9b79ea0830d74b0710f5d510b0bb3
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-13 00:06:35 -0700

    Reistricted middleware no longer checks for 'Bearer'

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -1,22 +1,18 @@
-// //this verifies the oauth access token
-// module.exports = {
-//   restricted
-// };
+//this verifies the oauth access token
+module.exports = {
+  restricted
+};
 
-// function restricted(req, res, next) {
-//   //get auth header value
-//   const bearerHeader = req.headers["authorization"];
-//   //check if bearer header is undefined
-//   if (typeof bearerHeader !== "undefined") {
-//     //split bearer header value "Bearer __token_here__" at the space in the string
-//     const bearer = bearerHeader.split(" ");
-//     //split returns an array. get token from array
-//     const bearerToken = bearer[1];
-//     //set token
-//     req.token = bearerToken;
-//     //pass baton to next middleware
-//     next();
-//   } else {
-//     res.sendStatus(403).json({ error: "forbidden" });
-//   }
-// }
+function restricted(req, res, next) {
+  //get auth header value
+  const authorizationToken = req.headers["authorization"];
+  //check if auth header is undefined
+  if (typeof authorizationToken !== "undefined") {
+    //set token
+    req.token = authorizationToken;
+    //pass baton to next middleware
+    next();
+  } else {
+    res.sendStatus(403).json({ error: "forbidden" });
+  }
+}
