45544b38adf7d9c4140cf018241803463d6021af
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-14 14:00:45 -0700

    deprecated middleware code, pushing in order to pull from development

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -3,18 +3,20 @@ module.exports = {
   restricted
 };
 
+// axios.get(`url`,
+//{headers: { authorization: token }} <- pass in this object for every axios call as the second argument
+//)
+
 function restricted(req, res, next) {
   //get auth header value
-  const authorizationToken = req.headers["authorization"];
+  const authToken = req.headers["authorization"];
   //check if auth header is undefined
-  if (typeof authorizationToken !== "undefined") {
+  if (typeof authToken !== "undefined") {
     //set token
-    req.token = authorizationToken;
+    req.token = authToken;
     //pass baton to next middleware
     next();
   } else {
     res.sendStatus(403).json({ error: "forbidden" });
   }
 }
-
-// implement passport.js verification method
