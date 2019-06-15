dc60c5ca4acc1bd002bbb7897e56a9a5e5626724
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-15 14:16:29 -0700

    fix xuser typo

--- routes/oauth/authenticate.js
+++ routes/oauth/authenticate.js
@@ -5,42 +5,38 @@ require("dotenv").config();
 
 router.post("/auth", async (req, res) => {
   let user = req.body;
-console.log (user,'authenticate at user assign')
+  console.log(user, "authenticate at user assign");
   // const xuser = await db.getByEmail(user.email);
   // console.log(xuser,'user exist?')
-  if (!await db.getByEmail(user.email)) {
+  if (!(await db.getByEmail(user.email))) {
     const newUser = {
       name: user.name,
       email: user.email,
       password: user.token
     };
     try {
-
-            const userO  = await db.insert(newUser);
-            res.status(200)
-            .json(newUser)
-            return;
-            }catch (error){
-                res.status(500).json({
-                   message: 'Error registering the User try alternative registering method'
-                })
-                return;
-
-  }}else{
+      const userO = await db.insert(newUser);
+      res.status(200).json(newUser);
+      return;
+    } catch (error) {
+      res.status(500).json({
+        message: "Error registering the User try alternative registering method"
+      });
+      return;
+    }
+  } else {
     try {
-        const udata = {
-          userId:xuser.userId,
-          password:user.password
-        }
-        res.status(200).json(udata);
-        return;
-        }catch (error){
-            res.status(500).json({
-               message: 'Error logging in the User try alternative login method'
-            })
- 
-  }
-
+      const udata = {
+        userId: user.userId,
+        password: user.password
+      };
+      res.status(200).json(udata);
+      return;
+    } catch (error) {
+      res.status(500).json({
+        message: "Error logging in the User try alternative login method"
+      });
+    }
   }
 });
 
