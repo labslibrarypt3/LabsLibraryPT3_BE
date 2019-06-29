b935460e8f6077d5658f14b8c64d20d15186d71b
iradsanchez <ira.d.sanchez@gmail.com>
2019-06-28 21:36:24 -0700

    debugging

--- routes/stripe/striperoutes.js
+++ routes/stripe/striperoutes.js
@@ -4,14 +4,13 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const request = require("request-promise");
 // const queryString = require("query-string");
 // const restricted = require("../../middleware/restricted");
-// router.use(require("body-parser").text());
 
 router.get("/connect", (req, response) => {
   const stripeData = {
     response_type: "code",
     uri: "https://connect.stripe.com/express/oauth/authorize",
     qs: {
-      redirect_uri: "https://localhost:3000/account",
+      redirect_uri: "https://goofy-mayer-45bb20.netlify.com/",
       client_id: "ca_FIasejiINwidFDyzoZ3EZ5Go8GKRfdsO"
     }
   };
--- routes/twilio/twilioChat.js
+++ routes/twilio/twilioChat.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const config = require('./config');
-const { chatToken, videoToken } = require('./tokens');
-const pino = require("express-pino-logger")();
+const config = require("./config");
+const { chatToken, videoToken } = require("./tokens");
+// const pino = require("express-pino-logger")();
 const bodyParser = require("body-parser");
 
 router.use(bodyParser.urlencoded({ extended: false }));
 router.use(bodyParser.json());
-router.use(pino);
+// router.use(pino);
 
 const sendTokenResponse = (token, res) => {
-  res.set('Content-Type', 'application/json');
+  res.set("Content-Type", "application/json");
   res.send(
     JSON.stringify({
       token: token.toJwt()
@@ -24,34 +24,32 @@ const sendTokenResponse = (token, res) => {
 //   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 // });
 
-
 // "./routes/twilio/twilioChat"
 
 router.get("/", (req, res) => {
   res.send("twilio up");
 });
 
-router.get('/token', (req, res) => {
+router.get("/token", (req, res) => {
   const identity = req.query.identity;
   const token = chatToken(identity, config);
   sendTokenResponse(token, res);
 });
 
-router.post('/token', (req, res) => {
+router.post("/token", (req, res) => {
   const identity = req.body.identity;
   const token = chatToken(identity, config);
   sendTokenResponse(token, res);
- 
 });
 
-router.get('/video/token', (req, res) => {
+router.get("/video/token", (req, res) => {
   const identity = req.query.identity;
   const room = req.query.room;
   const token = videoToken(identity, room, config);
   sendTokenResponse(token, res);
 });
 
-router.post('/video/token', (req, res) => {
+router.post("/video/token", (req, res) => {
   const identity = req.body.identity;
   const room = req.body.room;
   const token = videoToken(identity, room, config);
