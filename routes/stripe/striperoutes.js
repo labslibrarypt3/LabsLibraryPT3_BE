70c4d754716bf6ad6b7476abca1f7d72822b4350
Thomas Utsey <tom@edgegluer.com>
2019-07-29 21:45:58 -0400

    Code stripe route to handle token

--- routes/stripe/striperoutes.js
+++ routes/stripe/striperoutes.js
@@ -4,13 +4,50 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const request = require("request");
 // const restricted = require("../../middleware/restricted");
 
+const stateValue = Math.random()
+  .toString(36)
+  .slice(2);
+
+const stripeUrl = `https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://pt3-neighborhood-library-back.herokuapp.com/api/stripe/token/&client_id=ca_FIasejiINwidFDyzoZ3EZ5Go8GKRfdsO&state=${stateValue}&suggested_capabilities[]=platform_payments`;
+
 router.get("/connect", async (req, res) => {
   //this is used to authorize data back from stripe
-  const stateValue = Math.random()
-    .toString(36)
-    .slice(2);
-  const stripeUrl = `https://connect.stripe.com/express/oauth/authorize?redirect_uri=https://goofy-mayer-45bb20.netlify.com/&client_id=ca_FIasejiINwidFDyzoZ3EZ5Go8GKRfdsO&state=${stateValue}&suggested_capabilities[]=platform_payments`;
+
   res.redirect(stripeUrl);
+});
+
+router.get("/token", async (req, res) => {
+  console.log(req.session.state);
+  c;
+  // Check the `state` we got back equals the one we generated before proceeding (to protect from CSRF)
+  if (req.session.state != req.query.state) {
+    res.redirect(stripeUrl);
+  }
+  request.post(
+    config.stripe.tokenUri,
+    {
+      form: {
+        grant_type: "authorization_code",
+        client_id: config.stripe.clientId,
+        client_secret: config.stripe.secretKey,
+        code: req.query.code
+      },
+      json: true
+    },
+    (err, response, body) => {
+      if (err || body.error) {
+        console.log("The Stripe onboarding process has not succeeded.");
+      } else {
+        // Update the model and store the Stripe account ID in the datastore:
+        // this Stripe account ID will be used to issue payouts to the pilot
+        req.user.stripeAccountId = body.stripe_user_id;
+        req.user.save();
+      }
+      // Redirect to the Rocket Rides dashboard
+      // req.flash("showBanner", "true");
+      res.redirect("/stripe-connect-success");
+    }
+  );
 });
 
 module.exports = router;
